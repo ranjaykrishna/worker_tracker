f5cd099cfa3b811d75b9f972d58243cfb9cc9fb2
Ranjay Krishna <ranjaykrishna@gmail.com>
2016-05-23 14:24:19 -0700

    adding method to query for workers

--- workers/views.py
+++ workers/views.py
@@ -9,3 +9,13 @@ def index(request):
   workers = [w.tojson() for w in Worker.objects.all()]
   return render(request, 'index.html', {'data': json.dumps(workers)})
 
+def workerData(request):
+  if 'worker_id' not in request.GET:
+    return HttpResponse()
+  worker_id = request.GET['worker_id']
+  if not Worker.objects.filter(pk=worker_id).exists():
+    Worker.objects.create(worker_id=worker_id)
+  worker = Worker.objects.get(pk=worker_id)
+  return HttpResponse(json.dumps(worker.tojson()))
+
+
