5bd81af091c21b32798595026b69a669702203bf
Ranjay Krishna <ranjaykrishna@gmail.com>
2016-05-23 17:34:10 -0700

    adding hits class and allowing windows

--- workers/models.py
+++ workers/models.py
@@ -4,18 +4,35 @@ class Worker(models.Model):
   worker_id = models.SlugField(max_length=128, primary_key=True)
   condition = models.PositiveIntegerField(default=0)
   known = models.BooleanField(default=False)
+
+  def tojson(self, window=None):
+    if window == None:
+      hits = Hit.objects.filter(worker__pk=self.pk)
+    else:
+      hits = Hit.objects.filter(worker__pk=self.pk).order_by('-pk')[:window]
+    num_pos_gold = 0
+    num_pos_gold_correct = 0
+    num_neg_gold = 0
+    num_neg_gold_correct = 0
+    for hit in hits:
+      num_pos_gold += hit.num_pos_gold
+      num_pos_gold_correct += hit.num_pos_gold_correct
+      num_neg_gold += hit.num_neg_gold
+      num_neg_gold_correct += hit.num_neg_gold_correct
+    return {'worker_id': self.worker_id,
+      'condition': self.condition,
+      'known': self.known,
+      'num_pos_golds': num_pos_golds,
+      'num_neg_golds': num_neg_golds,
+      'num_pos_golds_correct': num_pos_golds_correct,
+      'num_neg_golds_correct': num_neg_golds_correct,
+      'num_hits': hits.count()}
+
+class Hit(models.Model):
+  hit_id = models.SlugField(max_length=128)
+  worker = models.ForeignKey(Worker)
   num_pos_golds = models.PositiveIntegerField(default=0)
   num_neg_golds = models.PositiveIntegerField(default=0)
   num_pos_golds_correct = models.PositiveIntegerField(default=0)
   num_neg_golds_correct = models.PositiveIntegerField(default=0)
-  num_hits = models.PositiveIntegerField(default=0)
 
-  def tojson(self):
-    return {'worker_id': self.worker_id,
-      'condition': self.condition,
-      'known': self.known,
-      'num_pos_golds': self.num_pos_golds,
-      'num_neg_golds': self.num_neg_golds,
-      'num_pos_golds_correct': self.num_pos_golds_correct,
-      'num_neg_golds_correct': self.num_neg_golds_correct,
-      'num_hits': self.num_hits}
--- workers/views.py
+++ workers/views.py
@@ -6,13 +6,19 @@ from .models import Worker
 import json
 
 def index(request):
-  workers = [w.tojson() for w in Worker.objects.all()]
+  window = None
+  if 'window' in request.GET:
+    window = request.GET['window']
+  workers = [w.tojson(window=window) for w in Worker.objects.all()]
   return render(request, 'index.html', {'data': json.dumps(workers)})
 
 def workerData(request):
   if 'worker_id' not in request.GET:
     return HttpResponse()
   worker_id = request.GET['worker_id']
+  window = 10
+  if 'window' in request.GET:
+    window = request.GET['window']
   if not Worker.objects.filter(pk=worker_id).exists():
     Worker.objects.create(worker_id=worker_id)
   worker = Worker.objects.get(pk=worker_id)
