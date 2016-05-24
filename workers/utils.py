ded0bf5c6a52c6eb87dedafae6e6209e102f180f
Ranjay Krishna <ranjaykrishna@gmail.com>
2016-05-24 00:39:56 -0700

    adding boto

--- workers/utils.py
+++ workers/utils.py
@@ -1,38 +1,40 @@
 from workers.models import *
 from boto.mturk.connection import MTurkConnection
 
+import json
+
 def get_mturk_connection_from_args():
   args = json.load(open('config.json'))
-  return MTurkConnection('mechanicalturk.amazonaws.com', **args)
+  return MTurkConnection(host='mechanicalturk.sandbox.amazonaws.com', aws_access_key_id=args['aws_access_key'], aws_secret_access_key= args['aws_access_key'])
+  #return MTurkConnection(host='mechanicalturk.amazonaws.com', aws_access_key_id=args['aws_access_key'], aws_secret_access_key= args['aws_access_key'])
 
-def approve(hit, attention_score=None, condition=None):
+def approve(hit, message):
   mtc = get_mturk_connection_from_args()
-  if score is None:
-    message = 'Good job. You passed all the attention checks.'
-  else:
-    message = 'You did not get all the attention checks. But your current score is %d\%. So you are still doing well. It\'s impossible to get all the attention checks. As long as you stay above the threshold of %d\%, you have nothing to worry about.' % (attention_score, condition)
-  mtc.approve_assignment(hit.assignment_id, message)
+  print hit.assignment_id, message
+  #mtc.approve_assignment(hit.assignment_id, message)
 
-def reject(hit, attention_score=None, condition=None):
+def reject(hit, message):
   mtc = get_mturk_connection_from_args()
-  message = 'You failed the attention checks.'
-  if score != None:
-    message += ' Your current score of %d\% dropped below the acceptance rate of %d\%.' % (attention_score, condition)
+  print hit.assignment_id, message
   mtc.reject_assignment(hit.assignment_id, message)
 
-def processHit():
+def processHits():
   WINDOW = 10
   hits = Hit.objects.filter(processed=False)
   for hit in hits:
-    old_hits = Hit.objects.filter(worker=hit.worker, processed=True).order_by('-pk')[:WINDOW]
+    old_hits = Hit.objects.filter(worker=hit.worker, pk__lt=hit.pk).order_by('-pk')[:WINDOW]
+    old_hits_to_process = []
     if float(hit.num_pos_golds_correct + hit.num_neg_golds_correct)/(hit.num_pos_golds + hit.num_neg_golds) == 1.0:
-      approve(hit)
+      message = 'Good job. You passed all the attention checks.'
+      approve(hit, message)
     elif old_hits.count() < WINDOW:
-      approve(hit)
+      continue
     else:
       num_correct = 0.0
       total = 0.0
       for old_hit in old_hits:
+        if not old_hit.processed:
+          old_hits_to_process.append(old_hit)
         num_correct += old_hit.num_pos_golds_correct
         num_correct += old_hit.num_neg_golds_correct
         total += old_hit.num_pos_golds
@@ -41,8 +43,19 @@ def processHit():
       if total > 0.0:
         score = 100.0*num_correct/total
       if score > hit.worker.condition:
-        approve(hit, attention_score=int(score), condition=hit.worker.condition)
+        message = 'You did not get all the attention checks. But your current score is %d\%. So you are still doing well. It\'s impossible to get all the attention checks. As long as you stay above the threshold of %d\%, you have nothing to worry about.' % (score, condition)
+        approve(hit, message)
+        for ohtp in old_hits_to_process:
+          approve(ohtp, message)
+          ohtp.processed = True
+          ohtp.save()
       else:
+        message = 'You did not pass the attention checks.'
+        message += ' Your current score of %d\% dropped below the acceptance rate of %d\%.' % (attention_score, condition)
         reject(hit, attention_score=int(score), condition=hit.worker.condition)
+        for ohtp in old_hits_to_process:
+          approve(ohtp, message)
+          ohtp.processed = True
+          ohtp.save()
     hit.processed = True
     hit.save()
