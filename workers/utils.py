7ade9fbce62ef9d5b2b2d829ca0e1dced0047b34
Ranjay Krishna <ranjaykrishna@gmail.com>
2016-05-24 02:28:29 -0700

    adding proper messages for different conditions

--- workers/utils.py
+++ workers/utils.py
@@ -39,7 +39,9 @@ def processHits():
     old_hits = Hit.objects.filter(worker=hit.worker, pk__lt=hit.pk).order_by('-pk')[:WINDOW]
     old_hits_to_process = []
     if hit.num_pos_golds_correct + hit.num_neg_golds_correct == hit.num_pos_golds + hit.num_neg_golds:
-      message = 'Good job. You passed all the attention checks.'
+      message = 'Good job. Keep going!'
+      if hit.worker.known:
+        message = 'Good job. You passed all the attention checks.'
       approve(hit, message)
     elif old_hits.count() < WINDOW:
       continue
@@ -57,12 +59,16 @@ def processHits():
       if total > 0.0:
         score = 100.0*num_correct/total
       if score > hit.worker.condition:
-        message = ('You did not get all the attention checks but your current score is %d%% correct. You are still doing well. It\'s impossible to get all the attention checks. As long as you stay above the threshold of %d%%, you have nothing to worry about.' % (int(score), hit.worker.condition))
+        message = 'Good job. Keep going!'
+        if hit.worker.known:
+          message = ('You did not get all the attention checks but your current score is %d%% correct. You are still doing well. It\'s impossible to get all the attention checks. As long as you stay above the threshold of %d%%, you have nothing to worry about.' % (int(score), hit.worker.condition))
         approve(hit, message)
         for ohtp in old_hits_to_process:
           approve(ohtp, message)
       else:
-        message = 'You did not pass the attention checks. Your current score of %d%% is below the acceptance rate of %d%%.' % (int(score), hit.worker.condition)
+        message = 'You are not performing well on the attention checks.'
+        if hit.worker.known:
+          message = 'You did not pass the attention checks. Your current score of %d%% is below the acceptance rate of %d%%.' % (int(score), hit.worker.condition)
         reject(hit, message)
         for ohtp in old_hits_to_process:
           reject(ohtp, message)
