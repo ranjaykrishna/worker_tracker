9682618b3aa71b338930903f66f2e0a17843b07d
Ranjay Krishna <ranjaykrishna@gmail.com>
2016-05-24 00:41:38 -0700

    adding script for processing hits

---
+++ workers/management/commands/process_hits.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from workers.models import *
+from workers.utils import *
+
+import time
+
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    while True:
+      processHits()
+      time.sleep(1)
