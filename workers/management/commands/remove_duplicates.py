64dc0da9bc505f3dbbf624de16ad4507aa88eb13
Ranjay Krishna <ranjaykrishna@gmail.com>
2016-05-24 14:23:39 -0700

    adding file to set duplicates to processed

---
+++ workers/management/commands/remove_duplicates.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from workers.models import *
+from workers.utils import *
+
+import time
+
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    hit_dict = {}
+    for hit in Hit.objects.all():
+      if hit.assignment_id in hit_dict:
+        hit.processed = True
+        hit.save()
+        continue
+      hit_dict[hit.assignment_id] = 1
